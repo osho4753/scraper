b1025913bf96269dd012c2646f702cf790923652
osho4753 <ramazanmamanov840@gmail.com>
2025-07-29 15:01:25 +0200

    driver update

--- scraper.py
+++ scraper.py
@@ -58,10 +58,14 @@ headers = {
 def init_driver():
     try:
         options = Options()
-        options.add_argument("--headless")
+        options.add_argument("--headless=new")
+        options.add_argument("--disable-gpu") 
+        options.add_argument("--enable-unsafe-swiftshader")
         options.add_argument("--no-sandbox")
-        options.add_argument("--disable-gpu")
         options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument(f"user-agent={HEADERS}")
+
         driver = webdriver.Chrome(options=options)
         logging.info("Chrome WebDriver initialized")
         return driver
