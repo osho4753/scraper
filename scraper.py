be2cad22b5d911c7eec052bbd4973f8748e3b64b
osho4753 <ramazanmamanov840@gmail.com>
2025-07-29 14:07:55 +0200

    driver update

--- scraper.py
+++ scraper.py
@@ -49,14 +49,15 @@ HEADERS = {
 
 def init_driver():
     options = webdriver.ChromeOptions()
-    options.add_argument("no-sandbox")
-    options.add_argument("headless")
-    options.add_argument("disable-gpu")
-    driverPath = "/usr/bin/chromedriver"
-    
-    driver = webdriver.Chrome(driverPath, chrome_options=options)
-    logging.info("Chrome WebDriver initialized")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
 
+    driverPath = "/usr/bin/chromedriver"
+    service = Service(driverPath)
+    driver = webdriver.Chrome(service=service, options=options)
+
+    logging.info("Chrome WebDriver initialized")
     return driver
 
 headers = {
